574b9aede742650f8b18eaa0c6384aacdfd819a6
Joshua <JCoshua360@gmail.com>
2021-08-16 16:58:16 -0500

    Completed first exercise

---
+++ HelloDungeons/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloDungeons
+{
+    class Game
+    {
+        public void Run()
+        {
+            //Defining and Initionaliziation
+            string charaterName = "Name";
+            int areaNumber = 1;
+            int playerHealth = 20;
+            int strength = 1;
+            bool gameOver = false;
+            float points = 0.0f;
+
+            //Start Screen and Charater Creation
+            Console.WriteLine("Enter your Name below.");
+            charaterName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello " + charaterName + ".");
+            Console.ReadLine();
+        }
+    }
+}
