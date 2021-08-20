329887b0b1818292d5c34014afac75b1e9467561
Joshua <JCoshua360@gmail.com>
2021-08-20 11:55:31 -0500

    Added function for Stats

--- HelloDungeons/Game.cs
+++ HelloDungeons/Game.cs
@@ -6,37 +6,144 @@ namespace HelloDungeons
 {
     class Game
     {
+        //Defining and Initionaliziation
+        string charaterName = "Player";
+        string sideCharaterName = "Aeos";
+        string className = "None";
+        string weaponName = "None";
+        int weaponDamage = 0;
+        int charLevel = 1;
+        int playerHealth = 100;
+        int assisantHealth = 100;
+        int assisantStrength = 3;
+        int strength = 5;
+        int attempts = 5;
+        bool gameOver = false;
+        float exp = 0.0f;
+        string input = "input";
+        bool validInput = false;
+        float damage = 0.0f;
+
         public void Run()
         {
-            //Defining and Initionaliziation
-            string charaterName = "Player";
-            string sideCharaterName = "Aeos";
-            string className = "None";
-            string weaponName = "None";
-            int weaponDamage = 0;
-            int areaNumber = 1;
-            int playerHealth = 100;
-            int assisantHealth = 100;
-            int assisantStrength = 3;
-            int strength = 5;
-            int attempts = 5;
-            bool gameOver = false;
-            float points = 0.0f;
-            string input = "input";
-            bool validInput = false;
-            float damage = 0.0f;
+            
 
-            //Game Over Check
-            if (playerHealth <= 0)
+            //Get Stats
+            void GetStats()
             {
-                gameOver = true;
+                Console.WriteLine(charaterName + "'s Stats");
+                Console.WriteLine("HP: " + playerHealth);
+                Console.WriteLine("Class: " + className);
+                Console.WriteLine("Level: " + charLevel);
+                Console.WriteLine("Exp: " + exp);
+                Console.WriteLine();
+                Console.WriteLine("Weapon: " + weaponName);
+                Console.WriteLine("Damage: " + weaponDamage);
+                Console.WriteLine("Strength: " + strength);
+            }
+            //Actually Show Stats
+            string OpenStats(string input)
+            {
+                
             }
 
-            if (gameOver == true)
+            int GetInput(string description, string option1, string option2, string option3)
             {
-                Console.Clear();
-            }
+                while (validInput == false)
+                {
+                    //Option Menu
+                    Console.WriteLine(description + "\n" +
+                                   "1." + option1 + "\n" +
+                                   "2." + option2 + "\n" +
+                                   "3." + option3 + "\n");
+                    Console.Write(">");
+                    input = Console.ReadLine();
+                    if (input == "1" || input.ToLower() == option1)
+                    {
+                        Console.WriteLine("Warrior HP: 75\n" +
+                                          "Warrior Strength:40\n" +
+                                          "Warrior Weapon: Promising Sword - 10 Dmg");
+                        Console.WriteLine("Do you want to proceed as a Warrior?");
+                        Console.Write(">");
+                        input = Console.ReadLine();
+                        if (input == "y" || input.ToLower() == "yes")
+                        {
+                            playerHealth = 75;
+                            strength = 40;
+                            className = "Warrior";
+                            weaponName = "Promising Sword";
+                            weaponDamage = 10;
+                            Console.WriteLine("'Oh, a Warrior. Neat.'");
+                            validInput = true;
+                        }
+                        else
+                        {
 
+                        }
+                    }
+                    else if (input == "2" || input.ToLower() == "archer")
+                    {
+                        Console.WriteLine("Archer HP: 90\n" +
+                                          "Archer Strength:25\n" +
+                                          "Archer Weapon: Promising Bow - 7 Dmg");
+                        Console.WriteLine("Do you want to proceed as an Archer?");
+                        Console.Write(">");
+                        input = Console.ReadLine();
+                        if (input == "y" || input.ToLower() == "yes")
+                        {
+                            playerHealth = 90;
+                            strength = 25;
+                            className = "Archer";
+                            weaponName = "Promising Bow";
+                            weaponDamage = 7;
+                            Console.WriteLine("'Oh, a ranger. Neat.'");
+                            validInput = true;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    else if (input == "3" || input.ToLower() == "tank")
+                    {
+                        Console.WriteLine("Tank HP: 150\n" +
+                                          "Tank Strength:10\n" +
+                                          "Tank Weapon: Promising Axe - 12 Dmg");
+                        Console.WriteLine("Do you want to proceed as a Tank?");
+                        Console.Write(">");
+                        input = Console.ReadLine();
+                        if (input == "y" || input.ToLower() == "yes")
+                        {
+                            playerHealth = 150;
+                            strength = 10;
+                            className = "Tank";
+                            weaponName = "Promising Axe";
+                            weaponDamage = 12;
+                            Console.WriteLine("'Oh, a Tank. Neat.'");
+                            validInput = true;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    else if (input.ToLower() == "no")
+                    {
+                        Console.WriteLine("Oh a rebel huh, thats fine.");
+                        Console.WriteLine("Class changed to Rebel.");
+                        Console.WriteLine("Rebel HP: 1\n" +
+                                      "Rebel Strength:1\n" +
+                                      "Tank Weapon: Rebel's Audacity - 100 Dmg");
+                        playerHealth = 1;
+                        strength = 1;
+                        className = "Rebel";
+                        weaponName = "Rebel's Audacity";
+                        weaponDamage = 100;
+                        validInput = true;
+                    }
+                    else Console.WriteLine("invalid input");
+                }
+            }
             //Start Screen and Charater Creation
             Console.WriteLine("Enter your Name below.");
             Console.Write(">");
@@ -169,99 +276,7 @@ namespace HelloDungeons
             Console.WriteLine("'So, who exactly are you?' They ask.\n" +
                 "Well obviously your " + charaterName + ", but what are you, like what do you do?");
             validInput = false;
-            while (validInput == false)
-            {
-                Console.WriteLine("Choose a class:\n" +
-                    "1. Warrior\n" +
-                    "2. Archer \n" +
-                    "3. Tank");
-                Console.Write(">");
-                input = Console.ReadLine();
-                if (input == "1" || input.ToLower() == "warrior")
-                {
-                    Console.WriteLine("Warrior HP: 75\n" +
-                                      "Warrior Strength:40\n" +
-                                      "Warrior Weapon: Promising Sword - 10 Dmg");
-                    Console.WriteLine("Do you want to proceed as a Warrior?");
-                    Console.Write(">");
-                    input = Console.ReadLine();
-                    if (input == "y" || input.ToLower() == "yes")
-                    {
-                        playerHealth = 75;
-                        strength = 40;
-                        className = "Warrior";
-                        weaponName = "Promising Sword";
-                        weaponDamage = 10;
-                        Console.WriteLine("'Oh, a Warrior. Neat.'");
-                        validInput = true;
-                    }
-                    else
-                    {
-
-                    }
-                }
-                else if (input == "2" || input.ToLower() == "archer")
-                {
-                    Console.WriteLine("Archer HP: 90\n" +
-                                      "Archer Strength:25\n" +
-                                      "Archer Weapon: Promising Bow - 7 Dmg");
-                    Console.WriteLine("Do you want to proceed as an Archer?");
-                    Console.Write(">");
-                    input = Console.ReadLine();
-                    if (input == "y" || input.ToLower() == "yes")
-                    {
-                        playerHealth = 90;
-                        strength = 25;
-                        className = "Archer";
-                        weaponName = "Promising Bow";
-                        weaponDamage = 7;
-                        Console.WriteLine("'Oh, a ranger. Neat.'");
-                        validInput = true;
-                    }
-                    else
-                    {
-
-                    }
-                }
-                else if (input == "3" || input.ToLower() == "tank")
-                {
-                    Console.WriteLine("Tank HP: 150\n" +
-                                      "Tank Strength:10\n" +
-                                      "Tank Weapon: Promising Axe - 12 Dmg");
-                    Console.WriteLine("Do you want to proceed as a Tank?");
-                    Console.Write(">");
-                    input = Console.ReadLine();
-                    if (input == "y" || input.ToLower() == "yes")
-                    {
-                        playerHealth = 150;
-                        strength = 10;
-                        className = "Tank";
-                        weaponName = "Promising Axe";
-                        weaponDamage = 12;
-                        Console.WriteLine("'Oh, a Tank. Neat.'");
-                        validInput = true;
-                    }
-                    else
-                    {
-
-                    }
-                }
-                else if (input.ToLower() == "no")
-                {
-                    Console.WriteLine("Oh a rebel huh, thats fine.");
-                    Console.WriteLine("Class changed to Rebel.");
-                    Console.WriteLine("Rebel HP: 1\n" +
-                                  "Rebel Strength:1\n" +
-                                  "Tank Weapon: Rebel's Audacity - 100 Dmg");
-                    playerHealth = 1;
-                    strength = 1;
-                    className = "Rebel";
-                    weaponName = "Rebel's Audacity";
-                    weaponDamage = 100;
-                    validInput = true;
-                }
-                else Console.WriteLine("invalid input");
-            }
+            
             Console.WriteLine(className);
                 Console.ReadKey();
             Console.Clear();
@@ -322,14 +337,77 @@ namespace HelloDungeons
                 Console.WriteLine("You enter a small room with a tightly sealed door infront of you.\n\n" +
                     sideCharaterName + " perks up, exclaming 'Oh, I know this puzzle, I think.\n" +
                     "Anyways you have to guess the correct order of the switches to open the doors ahead!'\n" +
-                    sideCharaterName + " is right, as you see five levers to your right all in random order.\n" +
+                    sideCharaterName + " is right, as you see five levers to your right all in random positions labeled 1 to 5.\n" +
                     "Be careful, you only get 5 tries");
+                Console.ReadKey();
                 Console.Clear();
                 for (int i=0; i<=attempts; i++)
                 {
+                    //Using Bools to change varables and write if statements easier
+                    //True = Closed - False = Open
+                    bool door1 = true;
+                    bool door2 = false;
+                    bool door3 = true;
+                    bool door4 = true;
+                    bool door5 = false;
                     int attemptsRemain = attempts - i;
-                    Console.WriteLine("Careful, you only have " + attemptsRemain + " attempts remaining");
-
+                    bool eventComplete = false;
+                    if (door1 == false && door2 == false && door3 == false && door4 == false && door5 == false)
+                    {
+                        eventComplete = true;
+                    }
+                    if (eventComplete == false)
+                    {
+                        Console.WriteLine("Careful, you only have " + attemptsRemain + " attempts remaining");
+                        if (door1)
+                        {
+                            Console.WriteLine("Door 1: Closed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Door 1: Open");
+                        }
+                        if (door2)
+                        {
+                            Console.WriteLine("Door 2: Closed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Door 2: Open");
+                        }
+                        if (door3)
+                        {
+                            Console.WriteLine("Door 3: Closed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Door 3: Open");
+                        }
+                        if (door4)
+                        {
+                            Console.WriteLine("Door 4: Closed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Door 4: Open");
+                        }
+                        if (door5)
+                        {
+                            Console.WriteLine("Door 5: Closed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Door 5: Open");
+                        }
+                        Console.WriteLine("\nPull one of Levers.");
+                        Console.Write(">");
+                        Console.ReadLine();
+                        
+                    }
+                    else
+                    {
+                        break;
+                    }
 
                 }
             }
